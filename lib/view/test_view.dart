aa9154bc44d9e48d1dbe0007f812c21d590d972a
Eslam_Hossam1 <eslamhossam.tech@gmal.com>
2024-08-07 06:49:54 +0300

    push notification by topic instead of token

--- lib/view/test_view.dart
+++ lib/view/test_view.dart
@@ -120,11 +120,27 @@ class _TestViewState extends State<TestView> {
       appBar: AppBar(
         title: Text('title'),
       ),
-      body: ElevatedButton(
-        child: Text('send message'),
-        onPressed: () async {
-          await sendMessage('hi', 'eslam');
-        },
+      body: Column(
+        children: [
+          ElevatedButton(
+            child: Text('subscribe sokkary'),
+            onPressed: () async {
+              await FirebaseMessaging.instance.subscribeToTopic('sokkary');
+            },
+          ),
+          ElevatedButton(
+            child: Text('unsubscripe  sokkary'),
+            onPressed: () async {
+              await FirebaseMessaging.instance.unsubscribeFromTopic("sokkary");
+            },
+          ),
+          ElevatedButton(
+            child: Text('send message'),
+            onPressed: () async {
+              await sendMessage('hi', 'eslam');
+            },
+          ),
+        ],
       ),
     );
   }
@@ -135,20 +151,19 @@ class _TestViewState extends State<TestView> {
       'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
       'Content-Type': 'application/json',
       'Authorization':
-          'Bearer ya29.c.c0ASRK0GZe0rWJG4V4XsMFmFam7aFfctkKCvcwffCn2QhEarNG8D5HLWYb7Pfd4JFU24TcNYMOyO31Sgt3pEoauujQ4CQXGIIlFDsSPPVI3Q-19nExV_Z0gjmEWhpxIc9m5A_fcnUqGtCWDXyVqgvMDAE3585_2yNLm9KhG_tgxNP7dnJxV0TrmuubS3eMxKLJEnsLsTUkB6JLdrSdWOEu94BVH-67aNypC9hH2BrKyNcV1PMvAOKPIDRBY78G12wCi7Z0C0YPNIIdfA7Vl-TbgsVP5Km98MZjIr9upjSBezmGatZyeNy_6wFDhUL1rZb8Ai2HX9OvC-BzDdzAY5iOy9GSnizSs18_E0zdogsE5tkObRfV27kvj7yjN385PFunp2MoVUuxqi6UmSwBSM3y0w1i9O05uj9ptVpfS2k1x1Ysbh-Fg3cdMQnk7-O_em3VwmfvX5sedF5JIVi33qrlsc7WgwslQfbuXyO_zUgIee6tiQU6hXYMBYyXl9drcwn-i50Fdofv5Bi4s9k7oRZVzBywezl_gJ78IV4cSB8ebavVn35pyyoMYwesjqfR7xpvo-Iikpx7hmpi052F87ZB7qpRQjJ-uWdm1Qjecp8bR8gOkV6Mvy2Zhwpu26tu4rrv6ut2dcXXw45qdh4Vz_2MIy6ZcnRIzu24jxbrSB-26dliS9ZXZuwcjOmWoWFFSwBM179b111oSp7ncSFbyBUfb9me3uuaFWc0YmtImvJqYsd4R_oy94s_lnq2kflscxyB3_tOuQ6mV-mIemmnUlnsRQ7dmFjBJJ3uUkojdcZZhz-s_7FI1Op_Jun-32hgVvUxiYJW5vvfo7Zaa4ommoM5t2xUk_hfUjZWxngoZsRcfbafUZ81Zr9SsnnVhnhOWFqhXnm5viouvm0shje8776Xrz_8r1wycVsp-_-xtBIjwwO2jihO7fF8F5Rx_kMlMx0zzp6i7s1IlUsJ49vOWVaJOJ9Qz0r0f3yBhpf_4JJk'
+          'Bearer ya29.c.c0ASRK0GZcsXb2zerA0MJHjacpF6l4fLRRK6Z-sgfJVT9ZXTNEB3DKaV_B_wS915GMH028INUFiTYAg2NjD_pQl5okvGDj8WClnHgVXFQ6uk3YIcTQ_IyLZhYHVtBdG18hLGhrqvj_3BXdHEcIO6HvlLQ0371URWx575kpqmV7MsAoCdDZ7Y5BTfUJzTtWHjh7oP_8Nmo__OfH6Tkul94YOk7jvvQ6ZNcvFBlcJtLIh298du-SBi5MJglAYifgHal3yisyYij4LX2RDr5jV4cV5J-GKrADwqKOeweLdPVYNC7JxZQAXgaRfVkJRD-9cyXDH_YJoToJXFyxXiYj8vtjXVx-KgOdE5wzX2w32nPkXsD6-LUsXIoqzK5JFAN387KUBkwwz-rkw_ekOuhd5iZ5jJnwYhok-V-ZxZMOuevUrSUgg4alYdkkjbV6v8sfQwYz53j31oe2IwMlxq5f0WI42eOxjQQzVB5opsqI2SBSrZeVo0lZaOpmSglbmg_QScgoVdQcweduM-fsfkXlQYjwWRvjX2FkySk4_YvhbrxIQy8b7n5Mg39dm2u_Iclj-qMyh-zzy9MYWXMn-m3tuSgtoQ3_sn8u8Js9FXBhJr_nvX387JQ4spavq6pJcVj_jIMSkQU1RQv_IjSvdVxokni9OkFW9zjx20UvYZOBrb2pZqBgz_9F2gp_Z3O3qYlVQkwyZROizftrFYi498c3nuO1BSzy8J9usMec9pq4keqVBbdXWj3mQnoo8-JXpcy7_qdwM4ptk3cwXztQQnW4Ww8V3zY48S1Qes4-Fn8MhjZwQ4zMwqzYeduzgcMtX-_791Sl_tld6nUJauF3t9JIJaJuiou2cmd5_a-50eqlel2JbVFMtl0VZX8zOm4pOb-Yr2Xle_277eOq3iIB8bV67nshI6jY61iBa4_cy-uBJuy54I9th8hpdbwir3aeFQxpfd71BlY-2O4bZ8m4UBwpfnQykcwud4MkRl8rt03X4Xxe'
     };
     var url = Uri.parse(
         'https://fcm.googleapis.com/v1/projects/e-commerce-app-2af68/messages:send');
 
     var body = {
       "message": {
-        "token":
-            "eSu9RFNhS_GUNvDwYPkJYd:APA91bFa1_vjn2hpQHQTpJ5a_TNVBnUyWpFHcO_buJJy9IxSZ1TZz9cmNfKS8JNft3FAvmSAG5pEuLvCs1jW2w0pN0zTpPPglXBSVgGwl0fJUYQFAKX-iuh6XtNTsqWQcYeZHWkhT8fs",
+        "topic": "sokkary",
         "notification": {
           "title": "Breaking News",
           "body": "New news story available."
         },
-        "data": {"story_id": "story_12345", "name": "El zalton", "type": "chat"}
+        "data": {"story_id": "story_12345", "name": "sokkary", "type": "chat"}
       }
     };
 
